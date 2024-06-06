260e51f15b6fdb12a0f0d51d184542e247f6813b
Tien Tran <tientran@TLGs-Macbook.local>
2024-06-06 20:37:34 +0700

    fix

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,9 +1,7 @@
-import { ArrowBackIcon } from "@chakra-ui/icons";
-import { Button, Flex, Text } from "@chakra-ui/react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Flex } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  const navigate = useNavigate();
   return (
     <Flex
       flexDirection={"column"}
@@ -11,19 +9,6 @@ const Layout = () => {
       width={"100dvw"}
       height={"100dvh"}
     >
-      <Button
-        display={"inline-flex"}
-        width={"max-content"}
-        gap="0.25rem"
-        alignItems={"center"}
-        variant={"primary"}
-        onClick={() => navigate("/")}
-      >
-        <ArrowBackIcon />
-        <Text color={"#fff"} fontSize={"0.875rem"}>
-          Quay lại
-        </Text>
-      </Button>
       <Outlet />
     </Flex>
   );
--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -6,10 +6,12 @@ export const NotFound = () => {
   return (
     <div className="pageWrap">
       <h1 className="title">Không tìm thấy trang</h1>
+      <div className="contentMain">
       <img src={NotFoundImage} alt="Not found" className="img" />
       <div className="help-text-block">
         <p className="help-text1">Có vẻ bạn đang đi lạc rồi</p>
         <p className="help-text2">Hãy quay lại trang chủ nhé</p>
+      </div>
       </div>
       <Link to="/" className="backBtn">
         Quay lại trang chủ
